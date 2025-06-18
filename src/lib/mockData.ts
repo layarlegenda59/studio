d1b44ffcb54f081c00ebd8f79cc210c3a93ee3fd
Dadi Rustiawan <layarlegenda59@gmail.com>
2025-06-18 03:39:45 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:

--- src/lib/mockData.ts
+++ src/lib/mockData.ts
@@ -46,7 +46,7 @@ export const mockProducts: Product[] = [
     id: 'prod2',
     name: 'Tas Selempang Wanita Elegan',
     category: 'Tas',
-    imageUrl: 'https://placehold.co/400x400.png',
+    imageUrl: 'https://images.unsplash.com/photo-1554745028-65db781f9fa7?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHw2fHx0YXMlMjB3YW5pdGF8ZW58MHx8fHwxNzUwMjE3OTUzfDA&ixlib=rb-4.1.0&q=80&w=1080',
     originalPrice: 750000,
     sizes: ['One Size'],
     salesCount: 85,
