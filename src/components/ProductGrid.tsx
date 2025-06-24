8271c0417eeb09b5ddf261111d2bef8cc4d9099f
Dadi Rustiawan <layarlegenda59@gmail.com>
2025-06-24 04:42:01 +0000

    produk cardnya terlalu berdempeta jaraknya coba atur lagi, dan ukurannya

--- src/components/ProductGrid.tsx
+++ src/components/ProductGrid.tsx
@@ -1,4 +1,3 @@
-
 "use client";
 
 import ProductCard from './ProductCard';
@@ -16,7 +15,7 @@ export default function ProductGrid({ products, onToggleWishlist, wishlistItems 
   }
 
   return (
-    <div className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-2 gap-y-4">
+    <div className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-4 gap-y-6">
       {products.map((product) => (
         <ProductCard 
           key={product.id} 
